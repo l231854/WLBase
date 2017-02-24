8b610a9b9f6d74961fc73c07c0c310d48e70c1a1
l231854 <1175095018@qq.com>
2017-02-24 09:04:27 +0800

    Initial Commit

---
+++ WLBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WLBase
+//
+//  Created by 雷王 on 17/2/24.
+//  Copyright © 2017年 WL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WLBase/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WLBase
+//
+//  Created by 雷王 on 17/2/24.
+//  Copyright © 2017年 WL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
